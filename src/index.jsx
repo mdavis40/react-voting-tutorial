41f2d61610b2856b6162cb0be558aa9351e850c9
Matthew Davis <mdavis0816@gmail.com>
2016-05-25 21:22:18 -0700

    wire up voting with reducer and action creators

--- src/index.jsx
+++ src/index.jsx
@@ -4,20 +4,18 @@ import {Router, Route, hashHistory} from 'react-router'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducer'
+import io from 'socket.io-client'
+import {setState} from './action_creators'
 import App from './components/App'
 import {VotingContainer} from './components/Voting'
 import {ResultsContainer} from './components/Results'
 
 const store = createStore(reducer)
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-})
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`)
+socket.on('state', state =>
+  store.dispatch(setState(state))
+)
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
